5b83d54baded42dfdcb476189744890e69b05965
Karim El Hoshy <karimelhoshy@macbook-stagiaire.home>
2024-09-13 11:24:51 -0400

    commiting tests

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+# For more information, please refer to https://aka.ms/vscode-docker-python
+FROM python:3.12.6
+
+EXPOSE 80
+
+# Keeps Python from generating .pyc files in the container
+ENV PYTHONDONTWRITEBYTECODE=1
+
+# Turns off buffering for easier container logging
+ENV PYTHONUNBUFFERED=1
+
+# Install pip requirements
+COPY requirements.txt .
+RUN python -m pip install --no-cache-dir -r requirements.txt
+
+WORKDIR /src/moovitamix_fastapi 
+COPY . .
+
+# Creates a non-root user with an explicit UID and adds permission to access the /app folder
+# For more info, please refer to https://aka.ms/vscode-docker-python-configure-containers
+# RUN adduser -u 5678 --disabled-password --gecos "" appuser && chown -R appuser /app
+# USER appuser
+
+# During debugging, this entry point will be overridden. For more information, please refer to https://aka.ms/vscode-docker-python-debug
+CMD ["uvicorn", "moovitamix_fastapi.main:app", "--host", "0.0.0.0", "--port", "80"]
--- test/test_classes_out.py
+++ test/test_classes_out.py
@@ -1,5 +1,7 @@
 import datetime
-from src.moovitamix_fastapi.classes_out import TracksOut, UsersOut, ListenHistoryOut, gender_list, genre_list
+from src.moovitamix_fastapi.classes_out import TracksOut, UsersOut, ListenHistoryOut, TopSongsOut, gender_list, genre_list
+import unittest
+from unittest.mock import patch
 
 # Testing TracksOut
 def test_tracks_out_generate_fake():
@@ -33,3 +35,51 @@ def test_listen_history_out_generate_fake():
     assert history.items is None
     assert isinstance(history.created_at, datetime.datetime)
     assert isinstance(history.updated_at, datetime.datetime)
+
+# Testing TopSongsOut
+# def test_top_songs_out_generate_fake():
+#     song = TopSongsOut.generate_fake()
+#     assert isinstance(song.id, int)
+#     assert isinstance(song.name, str)
+#     assert isinstance(song.artist, str)
+#     assert isinstance(song.songwriters, str)
+#     assert isinstance(song.duration, str)
+#     assert isinstance(song.genres, str)
+#     assert isinstance(song.album, str)
+#     assert isinstance(song.rank, int)
+#     assert isinstance(song.created_at, datetime.datetime)
+#     assert isinstance(song.updated_at, datetime.datetime)
+
+class TestTopSongsOut(unittest.TestCase):
+
+    @patch('src.moovitamix_fastapi.classes_out.TopSongsOut.generate_fake')
+    def test_top_songs_out_generate_fake(self, mock_generate_fake):
+
+        mock_generate_fake.return_value = TopSongsOut(
+            id=3981,
+            name="Yesterday",
+            artist="The Beatles",
+            songwriters="John Lennon",
+            duration="2:06",
+            genres="Pop",
+            album="Help!",
+            rank=19,
+            created_at=datetime.datetime.now(),
+            updated_at=datetime.datetime.now()
+        )
+        
+        song = TopSongsOut.generate_fake()
+        
+        assert isinstance(song.id, int)
+        assert isinstance(song.name, str)
+        assert isinstance(song.artist, str)
+        assert isinstance(song.songwriters, str)
+        assert isinstance(song.duration, str)
+        assert isinstance(song.genres, str)
+        assert isinstance(song.album, str)
+        assert isinstance(song.rank, int)
+        assert isinstance(song.created_at, datetime.datetime)
+        assert isinstance(song.updated_at, datetime.datetime)
+
+if __name__ == '__main__':
+    unittest.main()
